1f049fcb167dd2a0446ff9b8af085c5386641fa1
Naomi <naomimyselfandi@gmail.com>
2025-06-01 14:41:21 -0400

    Simplify ruleset access check

--- xanaduwars-backend/src/main/java/io/github/naomimyselfandi/xanaduwars/gameplay/rulesets/controller/RulesetController.java
+++ xanaduwars-backend/src/main/java/io/github/naomimyselfandi/xanaduwars/gameplay/rulesets/controller/RulesetController.java
@@ -1,15 +1,12 @@
 package io.github.naomimyselfandi.xanaduwars.gameplay.rulesets.controller;
 
-import io.github.naomimyselfandi.xanaduwars.account.value.Role;
-import io.github.naomimyselfandi.xanaduwars.auth.Authenticated;
-import io.github.naomimyselfandi.xanaduwars.auth.dto.UserDetailsDto;
 import io.github.naomimyselfandi.xanaduwars.gameplay.RulesetService;
 import io.github.naomimyselfandi.xanaduwars.gameplay.rulesets.dto.RulesetDtoMapper;
 import io.github.naomimyselfandi.xanaduwars.gameplay.value.Version;
 import io.github.naomimyselfandi.xanaduwars.gameplay.rulesets.dto.RulesetDto;
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /// A controller for reading information about rulesets.
@@ -22,21 +19,13 @@ public class RulesetController {
     private final RulesetDtoMapper rulesetDtoMapper;
 
     @GetMapping("/{version}")
-    public ResponseEntity<RulesetDto> get(
-            @Authenticated(required = false) @Nullable UserDetailsDto user,
-            @PathVariable Version version
-    ) {
-        if (version.isPublished() || isDeveloper(user)) {
-            var ruleset = rulesetService.load(version);
-            if (ruleset.isPresent()) {
-                return ResponseEntity.ok(rulesetDtoMapper.convert(ruleset.get()));
-            }
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    private boolean isDeveloper(@Nullable UserDetailsDto user) {
-        return user != null && user.getAuthorities().contains(Role.DEVELOPER);
+    @PreAuthorize("hasRole('DEVELOPER') || #version.isPublished()")
+    public ResponseEntity<RulesetDto> get(@PathVariable Version version) {
+        return rulesetService
+                .load(version)
+                .map(rulesetDtoMapper::convert)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
